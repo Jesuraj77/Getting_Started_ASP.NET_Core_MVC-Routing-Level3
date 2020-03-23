b93c4eabcd3e6260108a63a29b81e47c07dc5894
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 17:15:57 +0530

    Code Added

---
+++ Level3/Controllers/ArticleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Level3.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult article()
+        {
+            //Implement here
+            return View();
+        }
+    }
+}
